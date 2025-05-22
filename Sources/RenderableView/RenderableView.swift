5a6f7209063f5fa5d5a73e2fe0fe803979948b5b
yukiny0811 <yukiny0811@gmail.com>
2025-05-22 12:48:31 +0900

    equatable

--- Sources/RenderableView/RenderableView.swift
+++ Sources/RenderableView/RenderableView.swift
@@ -3,27 +3,37 @@
 
 import SwiftUI
 
-public struct Renderable<V: View>: View {
+public struct Renderable<V: View, E: Equatable>: View {
 
     @Environment(\.displayScale) private var displayScale
 
-    @Binding var updateCounter: Int
+    @Binding var trigger: E
     @Binding var renderedData: Data?
     let view: () -> V
 
     public init(
-        updateCounter: Binding<Int>,
+        updateCounter: Binding<E>,
         renderedData: Binding<Data?>,
         @ViewBuilder _ view: @escaping () -> V
     ) {
-        self._updateCounter = updateCounter
+        self._trigger = updateCounter
+        self._renderedData = renderedData
+        self.view = view
+    }
+
+    public init(
+        trigger: Binding<E>,
+        renderedData: Binding<Data?>,
+        @ViewBuilder _ view: @escaping () -> V
+    ) {
+        self._trigger = trigger
         self._renderedData = renderedData
         self.view = view
     }
 
     public var body: some View {
         view()
-            .onChange(of: updateCounter) {
+            .onChange(of: trigger) {
                 renderedData = render()
             }
     }
